9ea542401712e85108c0759f32bd25fb9f9329ec
c-satt <56098393+c-satt@users.noreply.github.com>
2022-08-27 19:30:35 -0700

    TIVIX-1: removed class, id, and element selctors to search.js file

---
+++ cypress/e2e/Constants/search.js
@@ -0,0 +1,19 @@
+const searchUserInput = {
+    country: '#country',
+    city: '#city',
+    model: '#model',
+    pickup: '#pickup',
+    dropoff: '#dropoff',
+    searchbtn: '.btn-primary',
+};
+
+const searchTable = {
+    table: '#search-results',
+    results: 'tbody',
+    individualRow: 'tr',
+};
+
+export {
+    searchUserInput,
+    searchTable,
+};
--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,14 +1,31 @@
+import {
+  searchUserInput,
+  searchTable,
+} from '../e2e/Constants/search';
+
 describe('Search page', () => {
   it('should filter avaliable cars by model when valid car model is entered in text field', () => {
     cy.visit('http://qalab.pl.tivixlabs.com');
-    cy.get('#country').select('Poland');
-    cy.get('#city').select('Wroclaw');
-    cy.get('#pickup').type('2022-09-15');
-    cy.get('#dropoff').type('2022-09-17');
-    cy.get('.btn-primary').contains('Search').click();
-    cy.get('#model').type('Aygo');
-    cy.get('.btn-primary').contains('Search').click();
-    let count = 0
-    cy.get('#search-results').children('tbody').children('tr').first().contains('Aygo');
+    cy.get(searchUserInput.country)
+      .select('Poland');
+    cy.get(searchUserInput.city)
+      .select('Wroclaw');
+    cy.get(searchUserInput.pickup)
+      .type('2022-09-15');
+    cy.get(searchUserInput.dropoff)
+      .type('2022-09-17');
+    cy.get(searchUserInput.searchbtn)
+      .contains('Search')
+      .click();
+    cy.get(searchUserInput.model)
+      .type('Aygo');
+    cy.get(searchUserInput.searchbtn)
+      .contains('Search')
+      .click();
+    cy.get(searchTable.table)
+      .children(searchTable.results)
+      .children(searchTable.individualRow)
+      .first()
+      .contains('Aygo');
   })
 })

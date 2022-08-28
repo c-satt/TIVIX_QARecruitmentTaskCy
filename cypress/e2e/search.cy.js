875ab5499f5449beaae5d6f46bcefe2248f2b99e
c-satt <56098393+c-satt@users.noreply.github.com>
2022-08-27 19:09:25 -0700

    TIVIS-1: Search by car model selects first car in list and sees if it contains the user input model

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,5 +1,14 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('http://qalab.pl.tivixlabs.com')
+describe('Search page', () => {
+  it('should filter avaliable cars by model when valid car model is entered in text field', () => {
+    cy.visit('http://qalab.pl.tivixlabs.com');
+    cy.get('#country').select('Poland');
+    cy.get('#city').select('Wroclaw');
+    cy.get('#pickup').type('2022-09-15');
+    cy.get('#dropoff').type('2022-09-17');
+    cy.get('.btn-primary').contains('Search').click();
+    cy.get('#model').type('Aygo');
+    cy.get('.btn-primary').contains('Search').click();
+    let count = 0
+    cy.get('#search-results').children('tbody').children('tr').first().contains('Aygo');
   })
 })
